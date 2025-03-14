fcff46859524349fdc91b93733114915c2e72a3d
Mifikus <mifikus322@gmail.com>
2025-03-14 21:37:53 +0300

    123

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import mysql.connector
 import requests
 import time
+from string import ascii_letters, digits
 
 
 connection = None
@@ -47,87 +48,105 @@ def get_xml() -> str:
     return requests.get(url, headers=HEADERS).text
 
 
+def fix_name(name: str) -> str:
+    """Фиксит запятые в имени"""
+    index = 0
+    new_name = ''
+    for _ in range(len(name)):
+        if index + 1 <= len(name):
+            if name[index] == ',':
+                if name[index+1] in ascii_letters or name[index+1] in digits:
+                    new_name += ', '
+            else:
+                new_name += name[index]
+        index += 1
+
+    return new_name
+
+
+
 while True:
-    try:
-        xml = get_xml()
-        root = ET.fromstring(xml)
+   try:
+       xml = get_xml()
+       root = ET.fromstring(xml)
 
-        ns = {"ns": "http://feed.pokerstars.com/TournamentFeed/2007"}
+       ns = {"ns": "http://feed.pokerstars.com/TournamentFeed/2007"}
 
-        for tournament in root.findall("ns:tournament", ns):
-            skip = False
-            for lobby in tournament.findall("ns:lobby[@type='COM']", ns):
-                if lobby.attrib['path'] == 'Tourney:Satellite:All' or lobby.attrib['path'] == 'Tourney:Freeroll':
-                    skip = True
-                    break
-            game = tournament.find("ns:game", ns).text
-            play_money = tournament.attrib.get('play_money')
-            if game != "Hold'em" or play_money == 'true' or skip:
-                continue
-            tournament_id = tournament.attrib['id']
-            name = tournament.find("ns:name", ns).text
-            gtd = '$' + str(name.replace(' ', '').split(',$')[-1].split('Gtd')[0])
-            name = name.split(', $')
-            if len(name) > 1:
-                name = ', $'.join(name[:-1])
-            else:
-                name = ''.join(name).replace('  ', '').split(',$')[0]
+       for tournament in root.findall("ns:tournament", ns):
+           skip = False
+           for lobby in tournament.findall("ns:lobby[@type='COM']", ns):
+               if lobby.attrib['path'] == 'Tourney:Satellite:All' or lobby.attrib['path'] == 'Tourney:Freeroll':
+                   skip = True
+                   break
+           game = tournament.find("ns:game", ns).text
+           play_money = tournament.attrib.get('play_money')
+           if game != "Hold'em" or play_money == 'true' or skip:
+               continue
+           tournament_id = tournament.attrib['id']
+           name = tournament.find("ns:name", ns).text
+           gtd = '$' + str(name.replace(' ', '').split(',$')[-1].split('Gtd')[0])
+           name = name.split(', $')
+           if len(name) > 1:
+               name = ', $'.join(name[:-1])
+           else:
+               name = ''.join(name).replace('  ', '').split(',$')[0]
 
-            date = tournament.find("ns:start_date", ns).text
-            buy_in = tournament.find("ns:buy_in_fee", ns).text
-            buy_in_values = buy_in.replace('$', '').replace(' ', '').split('+')
+           date = tournament.find("ns:start_date", ns).text
+           buy_in = tournament.find("ns:buy_in_fee", ns).text
+           buy_in_values = buy_in.replace('$', '').replace(' ', '').split('+')
 
-            # Преобразуем каждое значение в целое число и суммируем
-            total_buy_in_value = sum(float(value) for value in buy_in_values)
+           # Преобразуем каждое значение в целое число и суммируем
+           total_buy_in_value = sum(float(value) for value in buy_in_values)
 
-            if total_buy_in_value.is_integer():
-                total_buy_in_value = int(total_buy_in_value)
-                total_buy_in = f'${total_buy_in_value}'
-            else:
-                total_buy_in = f'${total_buy_in_value:.2f}'
-            # Форматируем итоговую строку
+           if total_buy_in_value.is_integer():
+               total_buy_in_value = int(total_buy_in_value)
+               total_buy_in = f'${total_buy_in_value}'
+           else:
+               total_buy_in = f'${total_buy_in_value:.2f}'
+           # Форматируем итоговую строку
 
-            amount_of_players = tournament.find("ns:max_table_players", ns).text
+           amount_of_players = tournament.find("ns:max_table_players", ns).text
 
-            for hyper_string, turbo_string, slow_string in zip(HYPER, TURBO, SLOW):
-                if hyper_string in name:
-                    speed = 'HYPER'
-                    break
-                if turbo_string in name:
-                    speed = 'TURBO'
-                    break
-                if slow_string in name:
-                    speed = 'SLOW'
-                    break
-            else:
-                speed = 'REG'
-            if 'Zoom' in name or 'Seats' in name or 'Phase' in name:
-                continue
-            if 'mystery' in name.lower() or 'Lotus' in name:
-                tournament_type = 'MYSTERY'
-            elif 'Bounty Adrenaline' in name or 'Bounty Builder' in name or 'Pacific Rim' in name or 'Progressive KO' in name:
-                tournament_type = 'KO'
-            else:
-                tournament_type = 'FREEZE'
+           for hyper_string, turbo_string, slow_string in zip(HYPER, TURBO, SLOW):
+               if hyper_string in name:
+                   speed = 'HYPER'
+                   break
+               if turbo_string in name:
+                   speed = 'TURBO'
+                   break
+               if slow_string in name:
+                   speed = 'SLOW'
+                   break
+           else:
+               speed = 'REG'
+           if 'Zoom' in name or 'Seats' in name or 'Phase' in name:
+               continue
+           if 'mystery' in name.lower() or 'Lotus' in name:
+               tournament_type = 'MYSTERY'
+           elif 'Bounty Adrenaline' in name or 'Bounty Builder' in name or 'Pacific Rim' in name or 'Progressive KO' in name:
+               tournament_type = 'KO'
+           else:
+               tournament_type = 'FREEZE'
 
-            output = (
-                f"Tournament ID: {tournament_id}\n"
-                f"Name: {name}\n"
-                f"Date: {date}\n"
-                f"Game: {game}\n"
-                f"Buy-in: {buy_in}\n"
-                f"Total Buy-in: {total_buy_in}\n"
-                f"Max Players: {amount_of_players}\n"
-                f"Speed: {speed}\n"
-                f"Tournament Type: {tournament_type}\n"
-            )
+           output = (
+               f"Tournament ID: {tournament_id}\n"
+               f"Name: {name}\n"
+               f"Date: {date}\n"
+               f"Game: {game}\n"
+               f"Buy-in: {buy_in}\n"
+               f"Total Buy-in: {total_buy_in}\n"
+               f"Max Players: {amount_of_players}\n"
+               f"Speed: {speed}\n"
+               f"Tournament Type: {tournament_type}\n"
+           )
 
-            print(output)
+           name = fix_name(name)
+           print(output)
 
-            if not is_there_tournament(tournament_id):
-                add_tournament(tournament_id, name, gtd, buy_in, total_buy_in, amount_of_players, speed, tournament_type, date)
-        print('Турниры кончились')
-        time.sleep(900)
-    except Exception as e:
-        time.sleep(10)
-        print(e)
+           if not is_there_tournament(tournament_id):
+               add_tournament(tournament_id, name, gtd, buy_in, total_buy_in, amount_of_players, speed, tournament_type, date)
+       print('Турниры кончились')
+       time.sleep(900)
+   except Exception as e:
+       time.sleep(10)
+       print(e)
